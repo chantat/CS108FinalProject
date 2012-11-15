e0cdbb5b65068ce87c67ede448cf5077f79348bf
Sameer_Arya <sarya@stanford.edu>
2012-11-14 17:59:44 -0800

    Added file for creating basic tables in database

---
+++ create.sql
@@ -0,0 +1,103 @@
+USE c_cs108_sarya;
+
+drop table if exists Question;
+create table Question(
+  qID int primary key,
+  qType int
+);
+
+drop table if exists QR;
+create table QR(
+  qID int,
+  qText char(255)
+);
+
+drop table if exists Answer;
+create table Answer(
+  qID int,
+  answerText char(255),
+  score decimal(6,4)
+);
+
+drop table if exists Quiz;
+create table Quiz(
+  quizID int primary key,
+  authorID int,
+  isRandomized bool,
+  prevID int,
+  isFlashcard bool,
+  allowsPractice bool,
+  immediateFeedback bool,
+  description char(255),
+  category char(255)
+);
+
+drop table if exists Tag;
+create table Tag(
+  quizID int,
+  tag char(255)
+);
+
+drop table if exists QuizQuestion;
+create table QuizQuestion(
+  quizID int,
+  qID int
+);
+
+drop table if exists QuizUser;
+create table QuizUser(
+  userID char(255) primary key,
+  password char(255),
+  isAdmin bool
+);
+
+drop table if exists Friend;
+create table Friend(
+  userID1 char(255),
+  userID2 char(255)
+);
+
+drop table if exists Message;
+create table Message(
+  fromID int,
+  toID int,
+  subject char(255),
+  messageText char(255),
+  status int,
+  messageTime timestamp
+);
+
+drop table if exists Challenge;
+create table Challenge(
+  quizID int,
+  fromID int,
+  toID int,
+  challengeTime timestamp,
+  status int
+);
+
+drop table if exists Request;
+create table Request(
+  fromID int,
+  toID int,
+  status int,
+  requestTime timestamp
+);
+
+drop table if exists Announcement;
+create table Announcement(
+  adminID int,
+  announcementText char(255),
+  subject char(255),
+  announcementTime timestamp
+);
+
+drop table if exists Rating;
+create table Rating(
+  userID int,
+  quizID int,
+  rating int,
+  ratingTime timestamp
+);
+
+insert into QuizUser values ('sarya', 'pass', 1);
