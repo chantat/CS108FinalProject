a94cb42fa55f2d0798f1e09883d62432e53f75ae
Pong Eksombatchai <chantat@stanford.edu>
2012-11-28 20:55:34 -0800

    Added Attempt class and a method to get all quiz attempts

--- src/userPackage/AccountUtil.java
+++ src/userPackage/AccountUtil.java
@@ -1,8 +1,8 @@
 package userPackage;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
+import quiz.Attempt;
 
 import webpackage.DBConnection;
 
@@ -67,5 +67,31 @@ public class AccountUtil {
 		}	
 		
 		return (hadHighScore == 1);
+	}
+	
+	public Attempt[] getAllQuizAttempt(String username) {
+		String command = "SELECT * FROM Attempts WHERE userID = \"" + username + "\";";
+		
+		Attempt ret[] = null;
+		ResultSet rs = null;
+		try {
+			rs = stmnt.executeQuery(command);
+			int numEntries = DBConnection.getResultSetSize(rs);
+			
+			ret = new Attempt[numEntries];
+			rs.first();
+			for (int i = 0; i < numEntries; i++) {
+				String userId = rs.getString("userID");
+				Integer quizId = rs.getInt("quizID");
+				Timestamp timeTaken = rs.getTimestamp("timeTaken");
+				
+				ret[i] = new Attempt(userId, quizId, timeTaken);
+				rs.next();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		
+		return ret;
 	}
 }
